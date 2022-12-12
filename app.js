70edb73bd6b707aa3cd345c2ce86c9acf1f1c872
J. Jackson <jjackson11211@gmail.com>
2022-12-12 13:55:53 -0500

    Minor changes

--- app.js
+++ app.js
@@ -7,16 +7,14 @@ inputField.addEventListener('keyup', (e) => {
     // if the user pressed entered btn and the input value is not empty
     if(e.key == 'Enter' && inputField.value != ""){
     //console.log('Hello, JavaScript!')
-    inputField.value = "";
     requestApi(inputField.value);
+    inputField.value = "";
     }
 });
 
-function requestApi(city) {
+function requestApi(city){
     let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=27cd174a3b303dec7b59e42eadf52e77`;
-    // the link shows up in the console
-    // console.log(api);
-    fetch(api).then(res => res.json()). then(result => weatherDetails(result))
+    fetch(api).then(res => res.json()).then(result => weatherDetails(result));
 }
 
 function weatherDetails(info) {
@@ -27,7 +25,7 @@ function weatherDetails(info) {
     const city = info.name;
     const country = info.sys.country;
     const {description, id} = info.weather[0];
-    const [feels_like, humidity, temp] = info.main;
+    const {feels_like, humidity, temp} = info.main;
 
     // let's pass these values to a particular html element
     
@@ -35,11 +33,21 @@ function weatherDetails(info) {
     container.querySelector('.temp .weather').innerText = description;
     container.querySelector('.location span').innerText = `${city} ${country}`;
     container.querySelector('.second-part .temp .number-2').innerText = Math.floor(feels_like);
-    container.querySelector('.column-1 .details span').innerText = `${humidity}`
+    container.querySelector('.column-1 .details span').innerText = `${humidity}`;
 
     // use custom icon according to the id which api returns us
 
     if (id == 800) {
-
+        weatherImg.src = "img/clear.png";
+    } else if (id >= 200 && id <= 232) {
+        weatherImg.src = "img/storm.png";
+    } else if (id >= 600 && id <= 622) {
+        weatherImg.src = "img/snow.png";
+    } else if (id >= 701 && id <= 781) {
+        weatherImg.src = "img/haze.png";
+    } else if (id >= 801 && id <= 804) {
+        weatherImg.src = "img/cloudy.png";
+    } else if ((id >= 300 && id <= 321) || (id >= 500 && id <= 531)) {
+        weatherImg.src = "img/rain.png";
     }
 }

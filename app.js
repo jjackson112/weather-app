7f4ac6b2c9f983c1e9131394aa254ad4e238b115
J. Jackson <jjackson11211@gmail.com>
2022-12-12 12:19:40 -0500

    JS
    weatherDetails

--- app.js
+++ app.js
@@ -17,3 +17,20 @@ function requestApi(city) {
     // console.log(api);
     fetch(api).then(res => res.json()). then(result => weatherDetails(result))
 }
+
+function weatherDetails(info) {
+    // key value pairs show up like clouds, humidity and timezones, etc
+    // console.log(info);
+
+    // let's get required properties from the info object
+    const city = info.name;
+    const country = info.sys.country;
+    const {description, id} = info.weather[0];
+    const [feels_like, humidity, temp] = info.main;
+
+    // let's pass these values to a particular html element
+    
+    container.querySelector('.temp .number').innerText = Math.floor(temp);
+    container.querySelector('.temp .weather').innerText = description;
+    container.querySelector('.temp .number').innerText
+}
